5fd03b31dbcb6d0e6e2388019c8d005382e6b895
Brad Green <bradgreen@infowest.com>
2014-04-21 14:51:11 -0600

    added blank directory structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link href="css/deepdive.css"  type="text/css" rel="stylesheet" />
+		<script src="js/deepdive.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<div id="text">
+			<?php
+			?>
+			<p></p>
+		</div>
+	</body>
+</html>
