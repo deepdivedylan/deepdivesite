b2e7c33eaecc4c875749a3cf02f5a411518946c3
Eric Debelak <ericdebelak@gmail.com>
2014-05-16 12:39:24 -0600

    user fix

--- blog/make_user.php
+++ blog/make_user.php
@@ -14,6 +14,7 @@ function makeUser()
     {
         $user = new User(-1, $email, $password, $salt);
         $user->insert($mysqli);
+        echo $user;
     }
     catch(Exception $exception)
     {
