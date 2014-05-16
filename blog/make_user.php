c7bca075284317b9d524436c682cbdbb68125bff
Eric Debelak <ericdebelak@gmail.com>
2014-05-16 12:27:43 -0600

    create a test user

---
+++ blog/make_user.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require_once("user.php");
+require_once("../../config.php");
+function makeUser()
+{
+    $mysqli = Pointer::getMysqli();
+    $email = "ericdebelak@gmail.com";
+    $salt = "136c67657614311f32238751044a0a3c0294f2a521e573afa8e496992d3786ba";
+    $password = "ilovesecurepasswords";
+    $password =  $password . $salt;
+    $password = hash("sha512", $password, false);
+    
+    
+}
+makeUser();
+?>
