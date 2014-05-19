891b00a3b42e6ab8b73b0f3b1f948b9a75ac8d45
Dylan McDonald <dylanm@deepdivecoders.com>
2014-05-19 16:26:08 -0600

    Refactored library to use the new config

--- apply/applylib.php
+++ apply/applylib.php
@@ -1,54 +1,28 @@
 <?php
-require_once("../../application.inc");
-require_once("aes.php");
+require_once("../../config.php");
 require_once("recaptchalib.php");
-
-/* connects to mySQL pointed to by the application.inc file
- * @returns pointer to the mySQL connection
- * @throws  if the connection cannot be established */
-function connectToMySQL()
-{
-	/* register globals */
-	global $AES256_KEY;
-	global $MYSQL_HOSTNAME;
-	global $MYSQL_USERNAME;
-	global $MYSQL_PASSWORD;
-	global $MYSQL_DATABASE;
-	
-	/* decrypt the password & connect */
-	$aes      = new AES($AES256_KEY);
-	$password = $aes->decrypt($MYSQL_PASSWORD);
-	$mysql    = new mysqli($MYSQL_HOSTNAME, $MYSQL_USERNAME, $password, $MYSQL_DATABASE);
-	
-	if($mysql->connect_error)
-	{
-		throw(new Exception("Unable to connect to mySQL: " . $mysql->connect_error));
-	}
-	
-	return($mysql);
-}
 
 /* posts application data to mySQL
  * @param  pointer to mySQL connection
  * @throws if an SQL or input validation error occurs */
-function postApplicationToMySQL($mysql)
+function postApplicationToMySQL(&$mysqli)
 {
 	$query = "INSERT INTO application(classId, aboutYourself, whyAttend, otherLinks, progExperience, fullName, phoneNumber, emailAddress, howHeard, skype, google, gender, birthday, browser, ipAddress) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	
+
 	/* prepare query */
-	$statement = $mysql->prepare($query);
+	$statement = $mysqli->prepare($query);
 	if($statement === false)
 	{
-		throw(new Exception("Unable to prepare mySQL query: " . $mysql->error));
+		throw(new Exception("Unable to prepare mySQL query: " . $mysqli->error));
 	}
-	
+
 	/* reformat IP address */
-	$ipAddress = ip2long($_SERVER["REMOTE_ADDR"]);
+	$ipAddress = inet_pton($_SERVER["REMOTE_ADDR"]);
 	if($ipAddress === false)
 	{
 		throw(new Exception("Unable to prepare mySQL query: invalid IP address"));
 	}
-	
+
 	/* verify & normalize the date */
 	$birthday = null;
 	try
@@ -60,7 +34,7 @@ function postApplicationToMySQL($mysql)
 	{
 		throw(new Exception("Unable to prepare mySQL query: invalid birthday", 0, $e));
 	}
-	
+
 	/* bind parameters to the prepared statement */
 	$statement->bind_param("isssssssssssssi",
 								$_POST["classId"],
@@ -78,13 +52,13 @@ function postApplicationToMySQL($mysql)
 								$birthday,
 								$_SERVER["HTTP_USER_AGENT"],
 								$ipAddress);
-	
+
 	/* execute mySQL query */
 	if($statement->execute() === false)
 	{
 		throw(new Exception("Unable to query mySQL: " . $statement->error));
 	}
-	
+
 	/* affected_rows != 1 implies the data wasn't able to be inserted */
 	if($statement->affected_rows !== 1)
 	{
@@ -95,27 +69,26 @@ function postApplicationToMySQL($mysql)
 /* emails an HTML formatted message with the application
  * @param  pointer to mySQL connection
  * @throws if an SQL or input validation error occurs */
-function emailApplicationResults($mysql)
+function emailApplicationResults(&$mysqli)
 {
-	global $DEST_EMAIL;
-	
 	/* set static variables */
-	$class   = getClassName($mysql, $_POST["classId"]);
-	$subject = "Application from " . $_POST["fullName"];
-	$from    = "Apache Web Server <" . strip_tags($_SERVER["SERVER_ADMIN"]) . ">";
-	
+	$destinationEmail = Pointer::getDestinationEmail();
+	$class            = getClassName($mysqli, $_POST["classId"]);
+	$subject          = "Application from " . $_POST["fullName"];
+	$from             = "Apache Web Server <" . strip_tags($_SERVER["SERVER_ADMIN"]) . ">";
+
 	/* build Email headers */
 	$headers  = "From: $from\r\n";
 	$headers .= "Reply-To: $from\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-	
+
 	/* set the preamble of the message */
 	$preamble  = "<html>\r\n<body>\r\n";
 	$preamble .= "<h1>Application From " . $_POST["fullName"] . "</h1>\r\n";
 	$preamble .= "<table border=\"3\">\r\n";
 	$preamble .= "<tr><th>Field</th><th>Answer</th>\r\n";
-	
+
 	/* build the table of values */
 	$message  = "<tr><td>Class</td><td>$class</td></tr>\r\n";
 	$message .= "<tr><td>Full Name</td><td>" . $_POST["fullName"] . "</td></tr>\r\n";
@@ -132,31 +105,31 @@ function emailApplicationResults($mysql)
 	$message .= "<tr><td>How Did You Hear About Us?</td><td>" . $_POST["howHeard"] . "</td></tr>\r\n";
 	$message .= "<tr><td>Browser</td><td>" . $_SERVER["HTTP_USER_AGENT"] . "</td></tr>\r\n";
 	$message .= "<tr><td>IP</td><td>" . $_SERVER["REMOTE_ADDR"] . "</td></tr>\r\n";
-	
+
 	/* close the table & message */
 	$postamble  = "</table>\r\n";
 	$postamble .= "</body>\r\n";
 	$postamble .= "</html>";
-	
+
 	/* Email the message */
 	$message = $preamble . $message . $postamble;
-	mail($DEST_EMAIL, $subject, $message, $headers);
+	mail($destinationEmail, $subject, $message, $headers);
 }
 
 /* gets a list of all available classes
  * @param   pointer to mySQL connection
  * @returns array of available classes
  * @throws  if an SQL or input validation error occurs */
-function getClassList($mysql)
+function getClassList(&$mysqli)
 {
 	/* prepare query */
 	$query = "SELECT id, className, startDate FROM classes WHERE startDate > DATE_ADD(NOW(), INTERVAL 1 WEEK)";
-	$statement = $mysql->prepare($query);
+	$statement = $mysqli->prepare($query);
 	if($statement === false)
 	{
-		throw(new Exception("Unable to prepare mySQL query: " . $mysql->error));
+		throw(new Exception("Unable to prepare mySQL query: " . $mysqli->error));
 	}
-	
+
 	/* execute mySQL query */
 	if($statement->execute() === false)
 	{
@@ -189,19 +162,19 @@ function getClassList($mysql)
  * @param   class ID to query
  * @returns string containing class name
  * @throws  if an SQL or input validation error occurs */
-function getClassName($mysql, $classId)
+function getClassName(&$mysqli, $classId)
 {
 	/* prepare query */
 	$query = "SELECT className, startDate FROM classes WHERE id = ?";
-	$statement = $mysql->prepare($query);
+	$statement = $mysqli->prepare($query);
 	if($statement === false)
 	{
-		throw(new Exception("Unable to prepare mySQL query: " . $mysql->error));
+		throw(new Exception("Unable to prepare mySQL query: " . $mysqli->error));
 	}
-	
+
 	/* bind parameters to the prepared statement */
 	$statement->bind_param("i", $classId);
-	
+
 	/* execute mySQL query */
 	if($statement->execute() === false)
 	{
@@ -225,24 +198,12 @@ function getClassName($mysql, $classId)
 	return($class);
 }
 
-/* retrieves and decrypts the private key for Captcha
- * @returns private key for Captcha API */
-function getCaptchaPrivateKey()
-{
-	global $AES256_KEY;
-	global $CAPTCHA_KEY;
-	
-	$aes = new AES($AES256_KEY);
-	$key = $aes->decrypt($CAPTCHA_KEY);
-	return($key);
-}
-
 /* verifies whether the captcha is valid
  * @throws if the captcha is invalid */
 function verifyCaptcha()
 {
-	$privateKey = getCaptchaPrivateKey();
-	$captcha = recaptcha_check_answer ($privateKey,
+	$privateKey = Pointer::getCaptchaKey();
+	$captcha    = recaptcha_check_answer ($privateKey,
 										$_SERVER["REMOTE_ADDR"],
 										$_POST["recaptcha_challenge_field"],
 										$_POST["recaptcha_response_field"]);
@@ -252,100 +213,5 @@ function verifyCaptcha()
 		/* if the captcha was entered incorrectly, display an error */
 		throw(new Exception("The reCAPTCHA wasn't entered correctly. Go back and try it again. (reCAPTCHA said: " . $captcha->error . ")"));
 	}
-}
-
-/* screen scrapes a WordPress page for the header and footer
- * @returns an array containing {header, footer} data
- * @throws  if a parse error occurs */
-function stealWordPressFormatting()
-{
-	/* first load a WordPress enabled page */
-	global $BASE_URL;
-	$html = file_get_contents($BASE_URL);
-	
-	/* define the header as <!DOCTYPE html>...<div id="content"> & add our own CSS */
-	$css          = "<link rel=\"stylesheet\" type=\"text/css\" href=\"apply.css\" />";
-	$beginNeedle  = "<div id=\"content\">";
-	$contentBegin = strpos($html, $beginNeedle);
-	if($contentBegin === false)
-	{
-		throw(new Exception("Unable to find WordPress header"));
-	}
-	$contentBegin = $contentBegin + strlen($beginNeedle);
-	$header       = substr($html, 0, $contentBegin);
-	$header       = str_replace("</head>", "$css\n</head>", $header);
-	
-	/* define a jQuery script to validate the application form */
-	$jQuery = <<<EOT
-<script type="text/javascript">
-jQuery("#applyForm").validate(
-{
-	rules:
-	{
-		aboutYourself:
-		{
-			required: true
-		},
-		whyAttend:
-		{
-			required: true
-		},
-		otherLinks:
-		{
-			required: true
-		},
-		progExperience:
-		{
-			required: true
-		},
-		fullName:
-		{
-			required: true
-		},
-		phoneNumber:
-		{
-			required: true
-		},
-		emailAddress:
-		{
-			required: true,
-			email: true
-		},
-		howHeard:
-		{
-			required: true
-		}
-	},
-	
-	messages:
-	{
-		aboutYourself:  "Please tell us about yourself",
-		whyAttend:      "Please tell us why you'd like to attend",
-		otherLinks:     "Please provide additional information about yourself",
-		progExperience: "Please tell us about your programming experience",
-		fullName:       "Please enter your full name",
-		phoneNumber:    "Please enter your phone number",
-		emailAddress:   "Please enter your Email address",
-		birthday:       "Please enter your birthday",
-		howHeard:       "Please tell us how you heard about us"
-	}
-});
-</script>
-EOT;
-
-	/* define the footer as <footer id="footer">...</html> & add the jQuery script */
-	$endNeedle   = "<footer id=\"footer\">";
-	$footerBegin = strpos($html, $endNeedle);
-	if($footerBegin === false)
-	{
-		throw(new Exception("Unable to find WordPress footer"));
-	}
-	$footer = "</div>\n</section>\n";
-	$footer = $footer . substr($html, $footerBegin);
-	$footer = str_replace("</body>", "$jQuery\n</body>", $footer);
-	
-	/* construct the array & return it */
-	$formatting = array($header, $footer);
-	return($formatting);
 }
 ?>
