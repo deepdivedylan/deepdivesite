9d24cf1cc53d04aa3a88feb8270b9fe5787a20cb
Dylan McDonald <dylanm@deepdivecoders.com>
2014-05-19 16:26:37 -0600

    Removed the stealing of WordPress formatting

--- apply/index.php
+++ apply/index.php
@@ -3,20 +3,17 @@
 require_once("applylib.php");
 $publicKey = "6LfJ8ekSAAAAAFz1jDyhvr9POl-UfASxN_yBbthB";
 
-$mysql = null;
+$mysqli = null;
 try
 {
-	$mysql      = connectToMySQL();
-	$classes    = getClassList($mysql);
-	$formatting = stealWordPressFormatting();
-	$mysql->close();
+	$mysqli     = Pointer::getMysqli();
+	$classes    = getClassList($mysqli);
 }
 catch(Exception $e)
 {
 	echo "Unable to connect to mySQL: " . $e->getMessage() . "<br />";
 	exit;
 }
-echo $formatting[0];
 ?>
 		<h1>Application</h1>
 		<p>You're a great fit for Deep Dive Coders because you're:</p>
@@ -39,11 +36,11 @@ echo $formatting[0];
 		</table>
 
 		<p><br />If you're ready to change your world, please tell us about you below.</p>
-		
+
 		<p><span class="required">* Required</span></p>
 
 		<p><span class="required">Please select the programming course you would like to attend. *</span></p>
-		
+
 		<form id="applyForm" method="post" action="send_application.php">
 			<p>
 				<select id="classId" name="classId">
@@ -58,7 +55,7 @@ echo $formatting[0];
 
 			<p><span class="required">Please tell us about yourself, including at least one personal story that helps describe you. *</span><br />
 			<textarea id="aboutYourself" name="aboutYourself" rows="8" placeholder="Tell us your story..." required></textarea></p>
-			
+
 			<p><span class="required">Why do you want to attend Deep Dive Coders? *</span><br />
 			<textarea id="whyAttend" name="whyAttend" rows="8" placeholder="Tell us why you want to learn with us..." required></textarea></p>
 
@@ -70,13 +67,13 @@ echo $formatting[0];
 
 			<p><span class="required">Full Name *</span><br />
 			<input class="textField" type="text" id="fullName" name="fullName" required /></p>
-			
+
 			<p><span class="required">Phone number *</span><br />
 			<input class="textField" type="text" id="phoneNumber" name="phoneNumber" required /></p>
 
 			<p><span class="required">Email address *</span><br />
 			<input class="textField" type="text" id="emailAddress" name="emailAddress" type="email" required /></p>
-			
+
 			<p><span class="required">How did you hear about Deep Dive Coders? *</span><br />
 			<input class="textField" type="text" id="howHeard" name="howHeard"  required /></p>
 
@@ -85,20 +82,19 @@ echo $formatting[0];
 
 			<p>Google+ ID (we actually like Google Hangouts better than Skype)<br />
 			<input class="textField" type="text" id="google" name="google" /></p>
-			
+
 			<p><span class="required">Gender *</span><br />
 				<select name="gender">
 					<option value="F">Female</option>
 					<option value="M">Male</option>
 				</select>
 			</p>
-			
+
 			<p><span class="required">Date of Birth *</span><br />
 			<input type="date" id="birthday" name="birthday" required /></p>
-			
+
 			<p><span class="required">Please enter the captcha below to show you're a human *</span><br />
 			<?php echo recaptcha_get_html($publicKey); ?></p>
-			
+
 			<p><input id="apply" name="apply" type="submit" value="Apply" />&nbsp;<input id="reset" name="reset" type="reset" value="Reset Form" /></p>
 		</form>
-<?php echo $formatting[1]; ?>
