b4045838463218e33d7db844d868b3bacb50f60d
Joshua Garcia <garcia.sparky@gmail.com>
2014-06-18 18:15:07 -0600

    added a new row of testimonials

--- index.php
+++ index.php
@@ -43,24 +43,6 @@
             <div class="carousel-caption">
               <h1>Stay Informed</h1>
               <p>Want to learn more? Feel free to call us at (505) 720-1380, email us at <a style="color:white; text-decoration:none" href="mailto:hello@deepdivecoders.com">hello@deepdivecoders.com</a> or join our mailing list below!</p>
-              <!--<form name="contact" method="post" action="send_email.php">
-                <table width="400" border="0" align="center" cellpadding="3" cellspacing="5">
-                  <tr>
-                    <td>Name:</td>
-                    <td><input name="name" type="text" id="name" size="50"></td>
-                  </tr>
-                  <tr>
-                    <td>Email:</td>
-                    <td><input name="email" type="text" id="email" size="50"></td>
-                  </tr>  
-                  <tr>
-                    <td>Phone:</td>
-                    <td><input name="phone" type="text" id="phone" size="50" value="email"></td>
-                  </tr>
-                  <tr>
-                    <td colspan="2"><input class="btn btn-default" type="submit" name="submit" value="Stay Informed"></td>
-                  </tr>
-                </table>-->
 		<form name="contact" method="post" action="php/send_contact_home.php">
 		<input name="email" type="text" style="color: black" id="email" size="30" placeholder="email@example.com">
 		<br/>
@@ -85,12 +67,31 @@
         <div class="col-lg-4">
           <img class="img-circle" src="images/kirsten.jpg" alt="Photo of Kirsten" height="200px" width="200px">
           <h2>Kirsten</h2>
-          <p>"Deep Dive Coders challenged me way beyond my level of comfort and tested my determination. With no prior technical skills, I am now looking forward to a career in web development. - Kirsten, Spring 2014</p>
+          <p>"Deep Dive Coders challenged me way beyond my level of comfort and tested my determination. With no prior technical skills, I am now looking forward to a career in web development." - Kirsten, Spring 2014</p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" src="images/matt.png" alt="Photo of Matt" height="200px" width="200px">
           <h2>Matt</h2>
           <p>"Deep Dive Coders was an intense, comprehensive learning experience. It isn't easy but it's worth it. I can now confidently analyze, interpret, and write computer code and it was amazing how much we were able to learn and retain by working through the various projects" -Matt, Fall 2013</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/josh.jpg" alt="Photo of Josh" height="200px" width="200px">
+          <h2>Josh</h2>
+          <p>"I would reccomend Deep Dive Coders to anyone that has the will and determination to be a programmer but just lacks the guided instruction. Everything was amazing, the course, the instructor, my classmates and the networking all are there to help you succeed." -Josh, Spring 2014</p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+      
+     <!-- adding another row of testimonials-->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/jordan.jpg" alt="Photo of Jordan" height="200px" width="200px">
+          <h2>Jordan</h2>
+          <p>"Blah blah blah boomtime" -Jordan, Winter 2013</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/calvin.png" alt="Photo of Calvin" height="200px" width="200px">
+          <h2>Calvin</h2>
+          <p>"Deep Dive Coders blah blah blah blah blah." -Calvin, Winter 2013</p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" src="images/josh.jpg" alt="Photo of Josh" height="200px" width="200px">
